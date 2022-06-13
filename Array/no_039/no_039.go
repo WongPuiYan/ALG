b07a4dbb181a9761568e0dbb07ecbe9acab220b6
Neo-Wong <843327849@qq.com>
2022-06-13 23:10:42 +0800

    2022/06/13: Daily ALG 039. Combination Sum

---
+++ Array/no_039/no_039.go
@@ -0,0 +1,36 @@
+package main
+
+import (
+	"fmt"
+)
+
+func combinationSum(candidates []int, target int) [][]int {
+	comb := []int{}
+	ans := [][]int{}
+	var dfs func(target, idx int)
+	dfs = func(target, idx int) {
+		if idx == len(candidates) {
+			return
+		}
+		if target == 0 {
+			ans = append(ans, append([]int(nil), comb...))
+			return
+		}
+		dfs(target, idx+1)
+		if target-candidates[idx] >= 0 {
+			comb = append(comb, candidates[idx])
+			dfs(target-candidates[idx], idx)
+			comb = comb[:len(comb)-1]
+		}
+	}
+	dfs(target, 0)
+	return ans
+}
+
+func main() {
+	// nums := [...]int{3}
+	nums := [...]int{2, 3, 6, 7}
+	target := 7
+	result := combinationSum(nums[:], target)
+	fmt.Println(result)
+}
