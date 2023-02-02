eace515a27e36205f746ba4592820802f04b41b7
neowong <dcneowong@qq.com>
2023-02-02 23:42:44 +0800

    leetcode 704

---
+++ binarySearch/leetcode_34.py
@@ -0,0 +1,25 @@
+"""
+给你一个按照非递减顺序排列的整数数组 nums，和一个目标值 target。请你找出给定目标值在数组中的开始位置和结束位置。
+
+如果数组中不存在目标值 target，返回 [-1, -1]。
+
+你必须设计并实现时间复杂度为 O(log n) 的算法解决此问题。
+
+来源：力扣（LeetCode）
+链接：https://leetcode.cn/problems/find-first-and-last-position-of-element-in-sorted-array
+著作权归领扣网络所有。商业转载请联系官方授权，非商业转载请注明出处。
+"""
+
+from typing import List
+
+
+class Solution:
+    def searchRange(self, nums: List[int], target: int) -> List[int]:
+        pass
+
+
+if __name__ == "__main__":
+    s = Solution()
+    nums = [5,7,7,8,8,10]
+    target = 8
+    print(s.searchRange(nums, target))
