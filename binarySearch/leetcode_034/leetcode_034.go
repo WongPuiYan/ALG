afbb7679747842211ab93e75588f683e60503776
neowong <dcneowong@qq.com>
2023-06-08 17:52:19 +0800

    binary search archive

---
+++ binarySearch/leetcode_034/leetcode_034.go
@@ -0,0 +1,26 @@
+/*
+给定一个排序数组和一个目标值，在数组中找到目标值，并返回其索引。如果目标值不存在于数组中，返回它将会被按顺序插入的位置。
+
+请必须使用时间复杂度为 O(log n) 的算法。
+
+来源：力扣（LeetCode）
+链接：https://leetcode.cn/problems/search-insert-position
+著作权归领扣网络所有。商业转载请联系官方授权，非商业转载请注明出处。
+*/
+
+package main
+
+import (
+	"fmt"
+)
+
+func searchRange(nums []int, target int) []int {
+
+	return nums
+}
+
+func main() {
+	nums := []int{5, 7, 7, 8, 8, 10}
+	target := 8
+	fmt.Println(searchRange(nums, target))
+}
